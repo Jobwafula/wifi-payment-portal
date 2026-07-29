2f79c3ceeda877a8d37216fe031ad4f1b73ef968
Jobwafula <jobwafulabg@gmail.com>
2026-07-29 14:00:01 +0300

    chages

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,6 @@ import Payments from "../components/payments/Payments";
 import Analytics from "../components/analytics/Analytics";
 import Devices from "../components/devices/Devices";
 import Settings from "../components/settings/Settings";
-import Logs from "../components/logs/Logs";
 import Support from "../components/support/Support";
 import Home from "../components/home/Home"; // Import the Home component
 
